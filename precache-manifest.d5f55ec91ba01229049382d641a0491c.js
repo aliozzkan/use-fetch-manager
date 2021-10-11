0558f8bfef42159f6e51609ce124f9a27e49ac96
aliozzkan <aliozzkan@gmail.com>
2021-10-11 13:44:10 +0300

    Updates

---
+++ precache-manifest.d5f55ec91ba01229049382d641a0491c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "719292ca061da4ebed1c417ec1da9795",
+    "url": "/use-fetch-manager/index.html"
+  },
+  {
+    "revision": "f4c0d9626bbd0ccebe47",
+    "url": "/use-fetch-manager/static/css/main.2c6a48fe.chunk.css"
+  },
+  {
+    "revision": "31ebb7f8fa38308b68ad",
+    "url": "/use-fetch-manager/static/js/2.d4d33973.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/use-fetch-manager/static/js/2.d4d33973.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4c0d9626bbd0ccebe47",
+    "url": "/use-fetch-manager/static/js/main.f2b06c63.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/use-fetch-manager/static/js/main.f2b06c63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6726111dcafc41ceb342",
+    "url": "/use-fetch-manager/static/js/runtime-main.84e1744d.js"
+  }
+]);
